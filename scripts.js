89db9736203265f54b08b7267e681f2fcf714b48
Kira Sl <42243887+KiraSl@users.noreply.github.com>
2019-08-21 20:58:56 +0100

    Change double quotes to single

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-const colors = [ "green", "blue", "pink", "purple", "teal", "red", "yellow", "orange"];
+const colors = [ 'green', 'blue', 'pink', 'purple', 'teal', 'red', 'yellow', 'orange'];
 
 colors.forEach(c => createElement(c));
+
 function createElement(c) {
   for ( let i = 0; i < 2; i++ ) {
-    let card = document.createElement('div');
+    const card = document.createElement('div');
     card.classList.add('card', `background-${c}`);
-    let container = document.querySelector('.container');
+    const container = document.querySelector('.container');
     container.appendChild(card);
   }
 }
