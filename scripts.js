523120d0eae52ce8f3f783a20cc85c743e0eb0ca
Kira Slobodina <slobodina.kira@gmail.com>
2019-08-28 20:36:02 +0100

    Create flipping cards

--- scripts.js
+++ scripts.js
@@ -1,12 +1,34 @@
-const colors = [ 'green', 'blue', 'pink', 'purple', 'teal', 'red', 'yellow', 'orange'];
+const colors = [
+  "green",
+  "blue",
+  "pink",
+  "purple",
+  "teal",
+  "red",
+  "yellow",
+  "orange"
+];
 
-colors.forEach(c => createElement(c));
+colors.forEach(color => createElement(color));
 
-function createElement(c) {
-  for ( let i = 0; i < 2; i++ ) {
-    const card = document.createElement('div');
-    card.classList.add('card', 'card-backside-color');
-    const container = document.querySelector('.container');
-    container.appendChild(card);
+function createElement(color) {
+  for (let i = 0; i < 2; i++) {
+    const flipCard = document.createElement("div");
+    flipCard.classList.add("flip-card");
+    const flipCardInner = document.createElement("div");
+    flipCardInner.classList.add("flip-card-inner");
+    const flipCardFront = document.createElement("div");
+    flipCardFront.classList.add("flip-card-front", `background-${color}`);
+    const flipCardBack = document.createElement("div");
+    flipCardBack.classList.add("flip-card-back");
+    const title = document.createElement("h1");
+    title.classList.add("title");
+    title.textContent = "Memory Game";
+    flipCardBack.appendChild(title);
+    flipCardInner.appendChild(flipCardBack);
+    flipCardInner.appendChild(flipCardFront);
+    flipCard.appendChild(flipCardInner);
+    const container = document.querySelector(".container");
+    container.appendChild(flipCard);
   }
 }
