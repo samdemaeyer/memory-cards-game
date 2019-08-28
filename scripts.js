80938932dc5f5c1fbf65c64953226342d5122052
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2019-08-28 19:33:11 +0100

    Change back-side color and make grid responsive (#7)

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ colors.forEach(c => createElement(c));
 function createElement(c) {
   for ( let i = 0; i < 2; i++ ) {
     const card = document.createElement('div');
-    card.classList.add('card', `background-${c}`);
+    card.classList.add('card', 'card-backside-color');
     const container = document.querySelector('.container');
     container.appendChild(card);
   }
