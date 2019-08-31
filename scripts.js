07debb5658d467ca3f51924bf54336a0927b1426
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2019-08-31 19:12:44 +0100

    Randomise the cards (#14)

--- scripts.js
+++ scripts.js
@@ -9,29 +9,37 @@ const colors = [
   'orange'
 ];
 
-colors.forEach(color => createElement(color));
+const allColors = [...colors, ...colors];
+
+function shuffle(array) {
+  array.sort(function() {
+    return Math.random() - 0.5;
+  });
+}
+
+shuffle(allColors);
+
+allColors.forEach(color => createElement(color));
 
 function createElement(color) {
-  for (let i = 0; i < 2; i++) {
-    const flipCard = document.createElement('div');
-    flipCard.classList.add('flip-card');
-    flipCard.addEventListener('click', function(clickEvent) {
-      clickEvent.currentTarget.classList.add('flipped');
-    });
-    const flipCardInner = document.createElement('div');
-    flipCardInner.classList.add('flip-card-inner');
-    const flipCardFront = document.createElement('div');
-    flipCardFront.classList.add('flip-card-front', `background-${color}`);
-    const flipCardBack = document.createElement('div');
-    flipCardBack.classList.add('flip-card-back');
-    const title = document.createElement('h1');
-    title.classList.add('title');
-    title.textContent = 'Memory Game';
-    flipCardBack.appendChild(title);
-    flipCardInner.appendChild(flipCardBack);
-    flipCardInner.appendChild(flipCardFront);
-    flipCard.appendChild(flipCardInner);
-    const container = document.querySelector('.container');
-    container.appendChild(flipCard);
-  }
+  const flipCard = document.createElement('div');
+  flipCard.classList.add('flip-card');
+  flipCard.addEventListener('click', function(clickEvent) {
+    clickEvent.currentTarget.classList.add('flipped');
+  });
+  const flipCardInner = document.createElement('div');
+  flipCardInner.classList.add('flip-card-inner');
+  const flipCardFront = document.createElement('div');
+  flipCardFront.classList.add('flip-card-front', `background-${color}`);
+  const flipCardBack = document.createElement('div');
+  flipCardBack.classList.add('flip-card-back');
+  const title = document.createElement('h1');
+  title.classList.add('title');
+  title.textContent = 'Memory Game';
+  flipCardBack.appendChild(title);
+  flipCardInner.appendChild(flipCardBack);
+  flipCardInner.appendChild(flipCardFront);
+  flipCard.appendChild(flipCardInner);
+  const container = document.querySelector('.container');
+  container.appendChild(flipCard);
 }
